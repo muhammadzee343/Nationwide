a520dde8fb01e2e4656afeec4ceeccae6b900b98
Shahzaib <shahzaib.naseer@piecyfer.com>
2022-12-13 12:36:55 +0500

    contact-us (InProgress)

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 function ContactUs(props: any) {
-  return <></>;
+  return (
+    <div className="flex justify-center">
+      <div className="w-full xl:container">
+        <div className="w-full flex pt-[82px]">
+          <div className="w-6/12 flex flex-col">
+            <div className="border-l-[8px] border-lime mb-[30px] px-5 py-2">
+              <h4 className="text-sm mb-[2px] mt-[5px] uppercase font-semibold text-[#1a1a1a] tracking-[1px]">
+                GET IN TOUCH
+              </h4>
+              <h2 className="text-[36px]  font-medium text-dark-blue font-semibold">
+                For All Enquiries
+              </h2>
+            </div>
+            <p></p>
+          </div>
+          <div className="w-6/12"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactUs;
