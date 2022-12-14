deb6af20339ab15158e548d90c498f6dbff1f868
Asad Ali <asadragepote812@gmail.com>
2022-12-14 16:46:41 +0500

    Adding show services functionality.

--- pages/order-now.tsx
+++ pages/order-now.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useRef, useState} from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import PropertyTypeCardComponent from "../components/propertyTypeCard.component";
 import { faBuilding, faHouse } from "@fortawesome/free-solid-svg-icons";
 import ButtonComponent from "../components/Button.component";
@@ -7,7 +7,7 @@ import Counter from "../components/counter";
 import RadioButton from "../components/RadioButton.component";
 import {
   contactOptions,
-  serviceAttributes,
+  attributes,
   services,
 } from "../utility/orderNowConstants";
 import TextField from "../components/TextFied.component";
@@ -36,6 +36,7 @@ function OrderNow({ commercialProperties, residentialProperties }) {
     gasFire: "",
     backBoiler: "",
     postCode: "",
+    hasGas:"",
     propertyAddress: "",
     contactAccess: "",
     contact: {
@@ -52,72 +53,188 @@ function OrderNow({ commercialProperties, residentialProperties }) {
     servicesSection.current?.scrollIntoView({ behavior: "smooth" });
   };
 
+  const setAllAttributes = (selectedServicesData : string[] ) =>{
+
+    let attributes: string[] = [...serviceAttributes];
+
+    const services =
+        propertyType === "Residential Property"
+            ? residentialProperties
+            : commercialProperties;
+    const addAttributes = services.forEach((data) => {
+      if (
+          selectedServicesData.includes(data.name) &&
+          data?.service_attributes
+      ) {
+        Object.keys(data?.service_attributes).forEach(function (key) {
+          console.log({ key });
+          attributes.push(key);
+        });
+      }
+    });
+
+    setServiceAttributes(attributes);
+  }
+
   const selectService = (title: string) => {
     const temp = title;
-    const services = propertyType === "Residential Property" ? residentialProperties : commercialProperties;
+    const services =
+      propertyType === "Residential Property"
+        ? residentialProperties
+        : commercialProperties;
     const condition = selectedService.findIndex((ele) => title === ele) === -1;
-    let attributes : string[] = [...serviceAttributes];
+    let attributes: string[] = [...serviceAttributes];
     if (condition) {
-      const selectedServicesData =[...selectedService, temp];
-      console.log({selectedServicesData});
-      const addAttributes = services.forEach((data) =>{
-        if ( selectedServicesData.includes(data.name)  && data?.service_attributes){
-          console.log("Came here");
-          Object.keys(data?.service_attributes).forEach(function(key) {
-            console.log({key})
-            attributes.push(key);
-          });
-        }
-      })
-      setSelectedService(selectedServicesData);
-      setServiceAttributes(attributes)
+      const selectedServicesData = [...selectedService, temp];
 
+      setAllAttributes(selectedServicesData);
+      setSelectedService(selectedServicesData);
     } else {
       const index = selectedService.findIndex((ele) => title === ele);
       selectedService.splice(index, 1);
-      const addAttributes = services.forEach((data) =>{
-        if ( selectedService.includes(data.name)  && data?.service_attributes){
-          Object.keys(data?.service_attributes).forEach(function(key) {
-            console.log({key})
-            attributes.push(key);
-          });
-        }
-      })
-
+      setAllAttributes(selectedService);
       setSelectedService([...selectedService]);
-      setServiceAttributes(attributes)
     }
   };
 
   const services = useMemo<JSX.Element[]>(() => {
-    const elements: JSX.Element[] = []
+    const elements: JSX.Element[] = [];
 
-    const services = propertyType === "Residential Property" ? residentialProperties : commercialProperties;
+    const services =
+      propertyType === "Residential Property"
+        ? residentialProperties
+        : commercialProperties;
 
     for (let i = 0; i < services.length; i++) {
       const service = services[i];
       elements.push(
-          <div
-              key={i}
-              className=" w-full md:w-5/12"
-              onClick={() => selectService(service.name)}
-          >
-            <ServiceSelectionCard
-                title={service.name}
-                className={`${
-                    selectedService.includes(service.name)
-                        ? "bg-lime self-end "
-                        : ""
-                }`}
-            />
-          </div>
-      )
+        <div
+          key={i}
+          className=" w-full md:w-5/12"
+          onClick={() => selectService(service.name)}
+        >
+          <ServiceSelectionCard
+            title={service.name}
+            className={`${
+              selectedService.includes(service.name) ? "bg-lime self-end " : ""
+            }`}
+          />
+        </div>
+      );
     }
 
-    return elements
-  }, [propertyType,selectedService])
+    return elements;
+  }, [propertyType, selectedService]);
 
-  console.log({ selectedService, serviceAttributes});
+  const serviceAttribute = useMemo<JSX.Element[]>(() => {
+    const elements: JSX.Element[] = [];
+
+    serviceAttributes.forEach((key, index) => {
+      const ele = attributes[key];
+
+      if (ele) {
+        elements.push(
+          <>
+            <div
+              className=" w-full sm:px-5 md:px-0 xl:w-8/12 mt-8 mb-2"
+              key={index}
+            >
+              <h3 className=" text-2xl md:text-3xl text-dark-blue font-bold my-[30px]">
+                {ele.headings}
+              </h3>
+            </div>
+            <div className="w-full flex sm:px-5 md:px-0 md:justify-center">
+              <div className="w-full md:w-8/12 flex flex-col md:flex-row flex-wrap gap-6 lg:justify-start">
+                {ele.options.map((attr, index) => {
+                  if (attr.type === "radio") {
+                    return (
+                      <>
+                        <div key={index} className="w-full md:w-5/12">
+                          <RadioButton
+                            title={attr.title}
+                            value={attr.value}
+                            lable={ele.attr}
+                            selectattribute={setAttributes}
+                            className={`${
+                              attribute[ele.attr] === attr.value
+                                ? "bg-lime"
+                                : "border border-grey-500"
+                            } border border-grey-500`}
+                          />
+                        </div>
+                      </>
+                    );
+                  } else {
+                    return (
+                      <div key={index} className="w-full  flex">
+                        <p className=" text-lg text-dark-blue font-semibold mr-3">
+                          {attr.title}
+                        </p>
+                        <Counter />
+                      </div>
+                    );
+                  }
+                })}
+                {ele.exactNumber && attribute[ele.attr] === "+" && (
+                  <div className="w-full flex">
+                    <p className=" text-lg text-dark-blue font-semibold mr-3 w-5/12">
+                      {ele.exactNumber}
+                    </p>
+                    <div className="w-5/12">
+                      <Counter />
+                    </div>
+                  </div>
+                )}
+                {ele.Alert && attribute[ele.attr] === "+" && <AlertBox text={ele.Alert} />}
+                {ele.radioQuestion.map((ques, index) => {
+                  return (
+                    <div key={index} className="flex flex-col w-full">
+                      <p className=" text-lg text-dark-blue my-3 font-semibold mr-3 w-5/12">
+                        {ques.question}
+                      </p>
+                      <div className="flex flex-wrap gap-9">
+                        {ques.options.map((x, index) => {
+                          return (
+                            <RadioButton
+                              key={index}
+                              title={x.title}
+                              value={x.value}
+                              lable={x.attr}
+                              selectattribute={setAttributes}
+                              className={`${
+                                  attribute[x.attr] === x.value
+                                      ? "bg-lime"
+                                      : "border border-grey-500"
+                              } border border-grey-500`}
+                            />
+                          );
+                        })}
+                      </div>
+                    </div>
+                  );
+                })}
+
+                {ele.exactNumber2 && attribute[ele.attr] === "+" && (
+                  <div className="w-full flex my-4">
+                    <p className=" text-lg text-dark-blue font-semibold mr-3 ">
+                      {ele.exactNumber2}
+                    </p>
+                    <div className="w-5/12">
+                      <Counter />
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </>
+        );
+      }
+    });
+
+    return elements;
+  }, [serviceAttributes, selectedService, attribute]);
+
+  console.log({ attribute });
 
   return (
     <>
@@ -181,94 +298,7 @@ function OrderNow({ commercialProperties, residentialProperties }) {
           {selectedService.length > 0 && (
             <section className="animate-fade-in-up" ref={servicesSection}>
               <div className="flex flex-col justify-center items-center px-5">
-                {/*{serviceAttributes.map((ele, index) => {*/}
-                {/*  return (*/}
-                {/*    <>*/}
-                {/*      <div*/}
-                {/*        className=" w-full sm:px-5 md:px-0 xl:w-8/12 mt-8 mb-2"*/}
-                {/*        key={index}*/}
-                {/*      >*/}
-                {/*        <h3 className=" text-2xl md:text-3xl text-dark-blue font-bold my-[30px]">*/}
-                {/*          {ele.headings}*/}
-                {/*        </h3>*/}
-                {/*      </div>*/}
-                {/*      <div className="w-full flex sm:px-5 md:px-0 md:justify-center">*/}
-                {/*        <div className="w-full md:w-8/12 flex flex-col md:flex-row flex-wrap gap-6 lg:justify-start">*/}
-                {/*          {ele.options.map((attr, index) => {*/}
-                {/*            if (attr.type === "radio") {*/}
-                {/*              return (*/}
-                {/*                <>*/}
-                {/*                  <div key={index} className="w-full md:w-5/12">*/}
-                {/*                    <RadioButton*/}
-                {/*                      title={attr.title}*/}
-                {/*                      selectattribute={setAttributes}*/}
-                {/*                      className={`${*/}
-                {/*                        attribute[ele.attr] === attr.title*/}
-                {/*                          ? "bg-lime"*/}
-                {/*                          : "border border-grey-500"*/}
-                {/*                      }border border-grey-500`}*/}
-                {/*                    />*/}
-                {/*                  </div>*/}
-                {/*                </>*/}
-                {/*              );*/}
-                {/*            } else {*/}
-                {/*              return (*/}
-                {/*                <div key={index} className="w-full  flex">*/}
-                {/*                  <p className=" text-lg text-dark-blue font-semibold mr-3">*/}
-                {/*                    {attr.title}*/}
-                {/*                  </p>*/}
-                {/*                  <Counter />*/}
-                {/*                </div>*/}
-                {/*              );*/}
-                {/*            }*/}
-                {/*          })}*/}
-                {/*          {ele.exactNumber && (*/}
-                {/*            <div className="w-full flex">*/}
-                {/*              <p className=" text-lg text-dark-blue font-semibold mr-3 w-5/12">*/}
-                {/*                {ele.exactNumber}*/}
-                {/*              </p>*/}
-                {/*              <div className="w-5/12">*/}
-                {/*                <Counter />*/}
-                {/*              </div>*/}
-                {/*            </div>*/}
-                {/*          )}*/}
-                {/*          {ele.Alert && <AlertBox text={ele.Alert} />}*/}
-                {/*          {ele.radioQuestion.map((ques, index) => {*/}
-                {/*            return (*/}
-                {/*              <div key={index} className="flex flex-col w-full">*/}
-                {/*                <p className=" text-lg text-dark-blue my-3 font-semibold mr-3 w-5/12">*/}
-                {/*                  {ques.question}*/}
-                {/*                </p>*/}
-                {/*                <div className="flex flex-wrap gap-9">*/}
-                {/*                  {ques.options.map((x, index) => {*/}
-                {/*                    return (*/}
-                {/*                      <RadioButton*/}
-                {/*                        key={index}*/}
-                {/*                        title={x.title}*/}
-                {/*                        className={`border border-grey-500 bg-lime`}*/}
-                {/*                      />*/}
-                {/*                    );*/}
-                {/*                  })}*/}
-                {/*                </div>*/}
-                {/*              </div>*/}
-                {/*            );*/}
-                {/*          })}*/}
-                
-                {/*          {ele.exactNumber2 && (*/}
-                {/*            <div className="w-full flex my-4">*/}
-                {/*              <p className=" text-lg text-dark-blue font-semibold mr-3 ">*/}
-                {/*                {ele.exactNumber2}*/}
-                {/*              </p>*/}
-                {/*              <div className="w-5/12">*/}
-                {/*                <Counter />*/}
-                {/*              </div>*/}
-                {/*            </div>*/}
-                {/*          )}*/}
-                {/*        </div>*/}
-                {/*      </div>*/}
-                {/*    </>*/}
-                {/*  );*/}
-                {/*})}*/}
+                {serviceAttribute}
                 <div className="w-full sm:px-5 md:px-0 xl:w-8/12 mt-8 mb-2">
                   <h3 className="text-2xl md:text-3xl text-dark-blue font-bold my-[30px]">
                     Property Information
@@ -368,12 +398,16 @@ OrderNow.getInitialProps = async () => {
   const res = await fetch("http://192.168.10.38:3000/services/list_services");
   const data = await res.json();
 
-  const commercialProperties = data.services.filter((data) => data.category === "commercial_property" );
-  const residentialProperties = data.services.filter((data) => data.category === "residential_property" );
+  const commercialProperties = data.services.filter(
+    (data) => data.category === "commercial_property"
+  );
+  const residentialProperties = data.services.filter(
+    (data) => data.category === "residential_property"
+  );
 
   return {
     commercialProperties,
-    residentialProperties
+    residentialProperties,
   };
 };
 
--- utility/orderNowConstants.tsx
+++ utility/orderNowConstants.tsx
@@ -7,8 +7,8 @@ export const contactOptions = [
   { type: "radio", title: "Other", value: "" },
 ];
 
-export const serviceAttributes = [
-  {
+export const attributes = {
+  "property_type": {
     headings: "Property Type:",
     attr: "propertyType",
     Alert: "",
@@ -16,38 +16,15 @@ export const serviceAttributes = [
     exactNumber2: "",
     radioQuestion: [],
     options: [
-      { type: "radio", title: "Mid Terraced House", value: "" },
-      { type: "radio", title: "End Terraced / Semi House", value: "" },
-      { type: "radio", title: "Detached House", value: "" },
-      { type: "radio", title: "Detached Bungalow", value: "" },
-      { type: "radio", title: "Semi Detached Bungalow", value: "" },
-      { type: "radio", title: "Flat / Maisonette / Other", value: "" },
+      {type: "radio", title: "Mid Terraced House", value: "mid_terraced_house"},
+      {type: "radio", title: "End Terraced / Semi House", value: "end_terraced_or_semi_house"},
+      {type: "radio", title: "Detached House", value: "detached_house"},
+      {type: "radio", title: "Detached Bungalow", value: "detached_bungalow"},
+      {type: "radio", title: "Semi Detached Bungalow", value: "semi_detached_bungalow"},
+      {type: "radio", title: "Flat / Maisonette / Other", value: "flat_or_maisonette_or_other"},
     ],
-    service_attributes: {
-      property_type: false,
-      property_age: false,
-      property_price: false,
-      property_area: false,
-      bedrooms: false,
-      studio: false,
-      other_rooms: false,
-      distribution_boards: false,
-      electrical_appliances: false,
-      floors: false,
-      supply_type: false,
-      gas_appliances: false,
-      gas_fire: false,
-      property_postcode: false,
-      property_address: false,
-      circuits: false,
-      session_id: false,
-      bathrooms: false,
-      pat_appliances: false,
-      fire_back_boiler: false
-    }
-
   },
-  {
+  "bedrooms":{
     headings: "No of Bedrooms",
     attr: "NoOfBedrooms",
     Alert:
@@ -59,13 +36,13 @@ export const serviceAttributes = [
     options: [
       { type: "radio", title: "Studio", value: "s" },
       { type: "radio", title: "1 Bedrooms", value: "1" },
-      { type: "radio", title: "2 Bedrooms", value: "2" },
+      { type: "radio", title: "2 Bed rooms", value: "2" },
       { type: "radio", title: "3 Bedrooms", value: "3" },
       { type: "radio", title: "4 Bedrooms", value: "4" },
-      { type: "radio", title: "5+ Bedrooms", value: "5" },
+      { type: "radio", title: "5+ Bedrooms", value: "+" },
     ],
   },
-  {
+  "distribution_boards":{
     headings: "How Many Fuse Boards You have in Property?",
     attr: "NoFuseBoard",
     Alert: "",
@@ -74,7 +51,7 @@ export const serviceAttributes = [
     radioQuestion: [],
     options: [{ type: "counter", title: "No of Fuse Board", value: "" }],
   },
-  {
+  "electrical_appliances":{
     headings: "Number of Electrical Appliances (PAT)",
     attr: "NoElectricalAppliances",
     Alert: "",
@@ -82,13 +59,13 @@ export const serviceAttributes = [
     exactNumber2: "",
     radioQuestion: [],
     options: [
-      { type: "radio", title: "1-10 Appliances", value: "" },
-      { type: "radio", title: "11-20 Appliances", value: "" },
-      { type: "radio", title: "21-30 Appliances", value: "" },
-      { type: "radio", title: "30+ Appliances", value: "" },
+      { type: "radio", title: "1-10 Appliances", value: "1-10" },
+      { type: "radio", title: "11-20 Appliances", value: "11-20" },
+      { type: "radio", title: "21-30 Appliances", value: "21-30" },
+      { type: "radio", title: "30+ Appliances", value: "+" },
     ],
   },
-  {
+  "property_age":{
     headings: "Property Age",
     attr: "PropertyAge",
     Alert: "",
@@ -96,11 +73,11 @@ export const serviceAttributes = [
     exactNumber2: "",
     radioQuestion: [],
     options: [
-      { type: "radio", title: "Pre 1940", value: "" },
-      { type: "radio", title: "Post 1940", value: "" },
+      { type: "radio", title: "Pre 1940", value: "pre_1940" },
+      { type: "radio", title: "Post 1940", value: "post_1940" },
     ],
   },
-  {
+  "property_price":{
     headings: "Property Price",
     attr: "PropertyPrice",
     Alert: "",
@@ -108,13 +85,13 @@ export const serviceAttributes = [
     exactNumber2: "",
     radioQuestion: [],
     options: [
-      { type: "radio", title: "Upto £100k", value: "" },
-      { type: "radio", title: "Upto £300k", value: "" },
-      { type: "radio", title: "Upto £600k", value: "" },
-      { type: "radio", title: "Upto £1M", value: "" },
+      { type: "radio", title: "Upto £100k", value: "100000" },
+      { type: "radio", title: "Upto £300k", value: "300000" },
+      { type: "radio", title: "Upto £600k", value: "600000" },
+      { type: "radio", title: "Upto £1M", value: "1000000" },
     ],
   },
-  {
+  "floors":{
     headings: "Number of Floors",
     attr: "NoFloors",
     Alert: "",
@@ -122,13 +99,13 @@ export const serviceAttributes = [
     exactNumber2: "",
     radioQuestion: [],
     options: [
-      { type: "radio", title: "1 Floor", value: "" },
-      { type: "radio", title: "2 Floors", value: "" },
-      { type: "radio", title: "3 Floors", value: "" },
-      { type: "radio", title: "4+ Floors", value: "" },
+      { type: "radio", title: "1 Floor", value: "1" },
+      { type: "radio", title: "2 Floors", value: "2" },
+      { type: "radio", title: "3 Floors", value: "3" },
+      { type: "radio", title: "4+ Floors", value: "+" },
     ],
   },
-  {
+  "gas_appliances":{
     headings: "Number of Gas Appliances (CP12)",
     attr: "NoGasAppliances",
     Alert:
@@ -138,27 +115,29 @@ export const serviceAttributes = [
     radioQuestion: [
       {
         question: "Do you have gas fire in the property?",
+        attr: "gasFire",
         options: [
-          { title: "Yes", value: "" },
-          { title: "No", value: "" },
+          { title: "Yes", value: true },
+          { title: "No", value: false },
         ],
       },
       {
         question: "Does this gas fire has a back-boiler?",
+        attr: "backBoiler",
         options: [
-          { title: "Yes", value: "" },
-          { title: "No", value: "" },
+          { title: "Yes", value: true },
+          { title: "No", value: false },
         ],
       },
     ],
     options: [
-      { type: "radio", title: "1 gas appliance", value: "" },
-      { type: "radio", title: "2 gas appliance", value: "" },
-      { type: "radio", title: "3 gas appliance", value: "" },
-      { type: "radio", title: "4+ gas appliance", value: "" },
+      { type: "radio", title: "1 gas appliance", value: "1" },
+      { type: "radio", title: "2 gas appliance", value: "2" },
+      { type: "radio", title: "3 gas appliance", value: "3" },
+      { type: "radio", title: "4+ gas appliance", value: "+" },
     ],
   },
-];
+};
 
 export const services = [
   "Electrical Installation Condition Report (EICR)",
