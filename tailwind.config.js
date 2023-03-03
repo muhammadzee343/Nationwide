8d58c33e6a5c21f3e8854e420cbf0514146b90b4
Asad Ali <asadragepote812@gmail.com>
2023-03-03 19:56:34 +0500

    Bug Fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,6 @@ module.exports = {
       backgroundImage: {
         home_bg_image: "url('../public/mainbg.jpg')",
         home_bg_dull_image: "url('../public/mainbg3.jpg')",
-        home_bg_image: "url('../public/mainbg2.jpg')",
         privacy_policy_image: "url('../public/privacy_image.jpg')",
         footer_bg_image: "url('../public/footer-bg.jpg')",
         header_bg: "url('../public/bg-header.jpg')",
