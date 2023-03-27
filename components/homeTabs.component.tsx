aa401c66bac782e37f3d99ad838111e6924863d5
Asad Ali <muhammad.adil@piecyfer.com>
2023-03-27 17:09:18 +0500

    Ui fixes.

--- components/homeTabs.component.tsx
+++ components/homeTabs.component.tsx
@@ -7,21 +7,21 @@ const HomeTab = () => {
     const [activeService, setService] = useState(LEC);
 
     return (
-        <div className="lg:flex gap-x-10 xl:gap-x-20 p-6 lg:py-3 lg:px-0 md:min-h-[421px]">
-            <div className="relative self-start lg:min-h-[450px] lg:bg-hero bg-cover lg:w-[50%] mb-5 lg:mb-0 ">
+        <div className="lg:flex gap-x-14 xxl:gap-x-16 xl:gap-x-[] p-6 lg:py-3 lg:px-0 md:min-h-[421px]">
+            <div className="relative self-start lg:min-h-[380px] xl:min-h-[450px] lg:bg-hero bg-cover lg:w-[50%] mb-5 lg:mb-0 ">
                 <ul className="flex justify-center flex-wrap align-center lg:flex-col lg:absolute -right-7 -bottom-[6%] z-10 lg:mt-6">
                     {SERVICES.map((ele, index) => {
                         return (
                             <li
-                                className="min-w-[143px] md:w-[300px] mr-3 mb-3 lg:mr-0 lg:mb-0 md:w-[230px] lg:w-[160px] xl:w-[230px]"
+                                className="min-w-[143px] md:w-[300px] mr-3 mb-3 lg:mr-0 lg:mb-0 md:w-[230px] lg:w-[180px] xl:w-[230px]"
                                 key={index}
                                 onClick={() => setService(ele.slug)}
                             >
                                 <div
-                                    className={`flex items-center px-3 border-white ${index !==0 ? "border-t-[1px]":"border-t-[0px]"} py-6 lg:min-h-[50px] xl:min-h-[80px] md:py-8  lg:rounded-none font-bold text-[11px] md:px-5 lg:px-3 xl:px-5 md:text-[17px]
-                                     lg:px-7 lg:py-6 lg:text-[13px] xl:text-[16px] ${
+                                    className={`flex items-center px-3 border-white ${index !==0 ? "border-t-[1px]":"border-t-[0px]"} py-6 lg:min-h-[50px] xl:min-h-[80px] md:py-8 lg:rounded-none font-bold text-[11px] md:px-5 lg:px-3 xl:px-5 md:text-[17px]
+                                     lg:px-7 lg:py-[17px] lg:text-[10px] xl:text-[16px] ${
                                         activeService.title === ele.slug.title
-                                            ? 'bg-lime'
+                                            ? 'bg-lime opacity-90'
                                             : 'bg-white text-black lg:bg-dark-blue opacity-90 lg:text-white hover:bg-lime hover:opacity-100'
                                     } w-full`}
                                 >
@@ -35,13 +35,12 @@ const HomeTab = () => {
                     })}
                 </ul>
             </div>
-
-            <div className="animate-fade-in-up flex flex-col md:px-11 lg:px-2 lg:mb-0 lg:w-[50%]">
-                <h2 className="text-[26px] md:text-[30px] font-bold leading-10 text-white lg:text-[20px] xl:text-[25px] lg:text-black mb-4 animate-fade-in-up">
+            <div className="animate-fade-in-up flex flex-col md:px-11 lg:px-1 lg:mb-0 lg:w-[50%]">
+                <h2 className="text-[26px] md:text-[30px] font-bold leading-10 text-white lg:text-[20px] xl:text-[25px] xxl:text-[34px] lg:text-black mb-6 animate-fade-in-up">
                     {activeService.title}
                 </h2>
                 <div className="flex flex-row">
-                    <h3 className="font-semibold text-3xl lg:text-2xl text-[#c2cf10] mb-5 animate-fade-in-up">
+                    <h3 className="font-semibold text-3xl lg:text-2xl xxl:[29px] text-[#c2cf10] mb-6 animate-fade-in-up">
                         {activeService.subTitle}
                     </h3>
                     <p className="text-[#c2cf10] mt-4">*</p>
@@ -49,7 +48,7 @@ const HomeTab = () => {
                 <ul className="marker:text-lime marker:text-[20px] list-disc text-white lg:text-black ml-5">
                     {activeService.points.map((ele, index) => {
                         return (
-                            <li key={index} className="text-sm lg:text-[13px] xl:text-[14px] p-2">
+                            <li key={index} className="text-sm lg:text-[13px] xl:text-[14px] mb-[12px] xl:mb-[16px]">
                                 {ele}
                             </li>
                         );
--- components/selectProperty.component.tsx
+++ components/selectProperty.component.tsx
@@ -18,30 +18,29 @@ export default function SelectProperty() {
     }, 1);
   };
   return (
-    <>
       <div
-        className="bg-white text-center border-transparent drop-shadow-xl rounded-full"
+        className="bg-white text-center border-transparent drop-shadow-xl lg:max-w-[382px] max-h-[400px]"
         id="main_quote_form"
       >
         <form id="instantquote_main" method="post">
-          <div className='bg-white rounded pb-6'>
-          <h3 className="text-white uppercase text-[26px] lg:text-[20px] xl:text-[26px] mx-25 bg-dark-blue py-[35px] px-[20px] block rounded drop-shadow-lg">
+          <div className='bg-white rounded'>
+          <h3 className="text-white uppercase text-[26px] lg:text-[20px] xl:text-[26px] mx-25 bg-dark-blue py-[35px] lg:py-[25px] xl:py-[35px] px-[20px] block rounded drop-shadow-lg">
             Get Instant Quote
           </h3>
-          <div className="mb-[25px] mt-0 py-[20px] px-[20px] h-[291px] text-center ">
+          <div className="mb-[25px] mt-0 px-[20px] h-[291px] xl:h-[320px] text-center ">
             <div className="text-left relative" id="default_screen">
-              <h6 className="text-black block text-base lg:text-sm xl:text-base my-[12px] text-center">
+              <h6 className="text-black block text-base lg:text-sm xl:text-base mt-[27px] mb-[25px] xl:mb-[31px] text-center">
                 Select Your Property as appropraite and get quote in 30 seconds!
               </h6>
-              <div className="justify-center h-[158px] flex-col">
+              <div className="justify-center  flex-col">
                 <div className="cat_box">
                   <label
-                    className="relative flex items-center justify-center border-2 border-lime py-[10px] xl:py-[16px] px-[5px]
-                   text-[15px] font-bold text-black cursor-pointer mb-[15px]
+                    className="relative flex items-center justify-center border-2 border-lime min-h-[51px] px-[5px]
+                    text-[14px] xxl:text-[20px]  text-black cursor-pointer mb-[20px]
                    hover:bg-lime"
                     onClick={() => displayDrawer("residential_property")}
                   >
-                    <Image src={Home} alt='home' className="h-7 w-7 mr-4"/>
+                    <Image src={Home} alt='home' className="h-8 w-8 mr-5"/>
                     {/*<FontAwesomeIcon*/}
                     {/*  className="w-8 text-black absolute left-[14px]"*/}
                     {/*  icon={faHouse}*/}
@@ -50,11 +49,11 @@ export default function SelectProperty() {
                   </label>
 
                   <label
-                    className="relative flex items-center justify-center border-2 border-lime py-[10px] xl:py-[16px] px-[5px]
-                    text-[14px] font-bold text-black cursor-pointer mb-[15px] hover:bg-lime"
+                    className="relative flex items-center justify-center border-2 border-lime min-h-[51px] px-[5px]
+                    text-[14px] xxl:text-[20px] text-black cursor-pointer mb-[20px] xl:mb-[37px] hover:bg-lime"
                     onClick={() => displayDrawer("commercial_property")}
                   >
-                    <Image src={Builiding} alt='home' className="h-7 w-7 mr-4"/>
+                    <Image src={Builiding} alt='home' className="h-7 w-7 mr-5"/>
                     {/*<FontAwesomeIcon*/}
                     {/*  className="w-[26px] ,w-3 text-black  absolute left-[14px]"*/}
                     {/*  icon={faBuilding}*/}
@@ -65,7 +64,7 @@ export default function SelectProperty() {
               </div>
               <ButtonComponent
                 text="Quote Now"
-                className="bg-lime text-black font-semibold uppercase px-[20px] py-[13px] hover:bg-dark-blue hover:text-white ease-in duration-200"
+                className="bg-lime text-black font-semibold uppercase px-[20px] min-h-[52px]  hover:bg-dark-blue hover:text-white ease-in duration-200"
                 onClick={() => displayDrawer("residential_property")}
                 type="button"
               />
@@ -74,6 +73,5 @@ export default function SelectProperty() {
           </div>
         </form>
       </div>
-    </>
   );
 }
