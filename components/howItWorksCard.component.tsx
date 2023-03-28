57a85d1f0f6b48a760a15eb0478485e051742430
Asad Ali <muhammad.adil@piecyfer.com>
2023-03-28 14:57:14 +0500

    Ui fixes.

--- components/howItWorksCard.component.tsx
+++ components/howItWorksCard.component.tsx
@@ -1,49 +1,35 @@
 import Image from 'next/image'
 
 export default function HowItWorks({
-  title = "",
-  paragraph = "",
-  index = 0,
-  image=null,
-  type = "",
-}) {
+                                     title = "",
+                                     paragraph = "",
+                                     index = 0,
+                                     image=null,
+                                     type = "",
+                                   }) {
   return (
-    <>
 
-      <div
-        className={`${
-          type === "service" ? "px-[6px] py-[5px]" : "md:px-[16px] py-[5px]"
-        } mt-8 md:mt-0 md:w-4/12 text-center drop-shadow-lg`}
-      >
 
-        <div
-          className={`${
-            type === "service"
-              ? "mb-[15px] px-[10px] pt-[12px] pb-[38px]"
-              : "mb-[15px] px-[10px] pt-[50px] pb-[38px]"
-          } bg-lime flex flex-col border-b-4 border-black justify-center rounded min-h-[340px] md:min-h-[455px] lg:min-h-[310px] min-w-[200px]`}
-        >
-            <div className='flex items-center justify-center w-24 h-24 rounded-full bg-white absolute self-center -top-12'>
-                <Image src={image} alt='order' className='w-[36px]'/>
-            </div>
-          <h2
-            className={`${
-              type === "service"
-                ? "text-[17px] mb-[8px]"
-                : "text-[20px] mb-[15px]"
-            } text-black font-bold uppercase`}
-          >
-            {title}
-          </h2>
-          <p
-            className={`${
-              type === "service" ? "text-[15px]" : "text-basic"
-            } text-black pl-2.5 pr-2.5`}
-          >
-            {paragraph}
-          </p>
-        </div>
+    <div
+      className={`relative text-center bg-lime flex flex-col justify-center border-b-2 border-black rounded-sm 
+          mt-8 md:mt-0 shadow-xl min-h-[280px] md:min-h-[325px] lg:min-h-[260px] xl:min-h-[230px] md:max-w-[240px] lg:max-w-[310px]
+          xl:max-w-[370px]`}
+    >
+      <div className='flex items-center justify-center drop-shadow-lg w-[93px] h-[93px] rounded-full bg-white absolute self-center -top-12'>
+        <Image src={image} alt='order' className={`${index!==1 ? "w-[50px] ":"w-[36px]" } `}/>
       </div>
-    </>
+      <h2
+        className="lg:mt-[60px] xl:mt-[60px] mb-[23px] text-[15px] lg:text-[17px] text-black font-bold uppercase"
+      >
+        {title}
+      </h2>
+      <p
+        className={`${
+          type === "service" ? "text-[13px]" : "text-[13px]"
+        } text-black px-[10%] h-[60%]`}
+      >
+        {paragraph}
+      </p>
+    </div>
   );
 }
