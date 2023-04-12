7e266ff7e81683b326b29c75f76ccf7888cb6b64
hafiz.zeeshan <hafiz.zeeshan@piecyfer.com>
2023-04-12 12:25:20 +0500

    ui fixes

--- pages/our-services.tsx
+++ pages/our-services.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {ourServices, howItWorks} from "../utility/constants";
+import {ourServices, howItWorks, homeServices} from "../utility/constants";
 import OurServicesComponent from "../components/ourServices.component";
 import Head from "next/head";
 import HowItWorks from "../components/howItWorksCard.component";
@@ -47,120 +47,124 @@ function OurServices(props: any) {
           </div>
 
           {/*SERVICE INFO*/}
-          <div className="w-full px-5 md:px-10 lg:px-0 py-5 lg:w-[950px] xl:min-w-[1170px] xl:max-w-[1170px] mx-auto">
-              <div className='border-b-4 rounded-full border-lime w-[110px] mb-3'></div>
-              <h2 className="text-black text-4xl font-semibold">
-                  All Our Services
-              </h2>
-              <div className="w-full flex flex-row ">
-                  <div className="w-full flex flex-row gap-x-2">
-                      <div className="flex flex-col justify-between items-center py-5 mb-8 ">
-                          <div className="w-[1px] items-center justify-between py-10 flex flex-col bg-dark-blue h-full">
-                              <div className="bg-lime rounded-full w-[5px] h-[8%]"></div>
-                              <div className="bg-lime rounded-full w-[5px] h-[8%]"></div>
-                              <div className="bg-lime rounded-full w-[5px] h-[8%]"></div>
-                              <div className="bg-lime rounded-full w-[5px] h-[8%]"></div>
-                          </div>
+
+          <div className="relative w-full xxl:max-w-[1440px] xxl:mx-auto mt-8">
+              <div className="relative w-full flex flex-row justify-center">
+                  <div className="flex flex-col justify-between items-center py-5 mb-8 lg:w-[2%] xxl:w-[1%] hidden md:block">
+                      <div className="w-[1px] items-center justify-between py-10 flex flex-col bg-dark-blue h-full">
+                          <div className="bg-lime rounded-full w-[5px] h-[8%]"></div>
+                          <div className="bg-lime rounded-full w-[5px] h-[8%]"></div>
+                          <div className="bg-lime rounded-full w-[5px] h-[8%]"></div>
+                          <div className="bg-lime rounded-full w-[5px] h-[8%]"></div>
                       </div>
-                      <div className="w-full flex flex-col items-center">
-                          {
-                              ourServices.map(({ list, title, image }, index) => {
-                                  return (
-                                    <div
-                                      className={`w-full ${
-                                        index % 2 != 0
-                                          ? "flex-col-reverse lg:flex-row"
-                                          : "flex-col-reverse  lg:flex-row-reverse"
-                                      } pb-[60px] flex gap-6`}
-                                      key={index}
-                                    >
-                                        <OurServices2 title={title} image={image} list={list}/>
-                                    </div>
-                                  );
-                              })}
+                  </div>
+                  <div className="flex justify-center lg:justify-start xl:justify-center content-container-homepage lg:w-[93%] xxl:w-[95%]">
+                      <div className="w-[85%] lg:min-[970px] xl:min-w-[1170px] xl:max-w-[1170px] mx-auto">
+                          <div className="flex flex-col items-center justify-center ">
+                              <div className='flex flex-col self-start mb-6'>
+                                  <div className='border-lime border-b-4 rounded-full w-[40%] mb-3'></div>
+                                  <h2 className="text-black text-4xl font-semibold">
+                                      All Our Services
+                                  </h2>
+                              </div>
+                              {
+                                  ourServices.map(({ list, title, image }, index) => {
+                                      return (
+                                          <div
+                                              className={`w-full ${
+                                                  index % 2 != 0
+                                                      ? "flex-col-reverse lg:flex-row"
+                                                      : "flex-col-reverse  lg:flex-row-reverse"
+                                              } pb-[60px] flex gap-6`}
+                                              key={index}
+                                          >
+                                              <OurServices2 title={title} image={image} list={list}/>
+                                          </div>
+                                      );
+                                  })}
+                          </div>
                       </div>
                   </div>
               </div>
           </div>
 
+          {/*ALL OUR SERVICES*/}
 
-          <div className="w-full py-5 lg:min-[970px] xl:min-w-[1170px] xl:max-w-[1170px] mx-auto">
-              {/*ALL OUR SERVICES*/}
+          <div className="w-full py-5 lg:min-[970px] xl:min-w-[1170px] xl:max-w-[1170px] mx-auto ">
               <section>
                   <div className="w-full flex flex-col items-start md:items-center justify-center px-3">
                       <div className="w-full md:container flex flex-wrap pb-[60px]">
-                          <div className="mb-[30px]  py-2">
-                              <div className='border-lime rounded-full border-b-4 w-[40%] mb-3'></div>
-                              <h2 className="text-black text-4xl font-semibold ">
+                          <div className="mb-[30px] pr-5 py-2">
+                              <div className='border-lime border-b-4 rounded-full w-[40%] mb-3'></div>
+                              <h2 className="text-black text-4xl font-medium font-semibold">
                                   All Our Services
                               </h2>
                           </div>
                           <div className="hidden lg:flex w-full flex-row justify-between">
                               {ourServices.map(({ list, title, image }, index) => {
                                   return (
-                                    <OurServicesComponent
-                                      key={index}
-                                      list={list}
-                                      title={title}
-                                      image={image}
-                                    />
+                                      <OurServicesComponent
+                                          key={index}
+                                          list={list}
+                                          title={title}
+                                          image={image}
+                                      />
                                   );
                               })}
                           </div>
                           <div className="lg:hidden w-full">
                               <CarouselComponent
-                                list={ourServices}
-                                component={OurServicesComponent}
-                                className="h-[420px]"
+                                  list={ourServices}
+                                  component={OurServicesComponent}
+                                  className="h-[470px]"
                               />
                           </div>
                       </div>
                   </div>
               </section>
-          </div>
 
-          <BoxBackgroundComponent className='h-[650px] pt-5'
-                                  greyStyle='w-[40%] h-[50%] md:w-[40%] hidden md:block'
-                                  limeStyle='right-0 bottom-0 w-[60%] h-[40%] md:w-[60%] hidden md:block'
-          >
-              <div className='w-full py-5 mt-3 lg:w-[970px] xl:min-w-[1170px] xl:max-w-[1170px] mx-auto'>
-                  <div className='px-2 ml-4 md:ml-0'>
-                      <div className='border-[2px] rounded-full w-[70px] mt-2 mb-1 md:w-28 border-lime'/>
-                      <h2 className='text-[18px] font-semibold md:text-[30px] mb-4'>Services Bundle Offers</h2>
+              <BoxBackgroundComponent className='h-[650px] pt-5'
+                                      greyStyle='w-[40%] h-[50%] md:w-[40%] hidden md:block'
+                                      limeStyle='right-0 bottom-0 w-[60%] h-[40%] md:w-[60%] hidden md:block'
+              >
+                  <div className='w-full py-5 mt-3 lg:w-[970px] xl:min-w-[1170px] xl:max-w-[1170px] mx-auto'>
+                      <div className='px-2 ml-4 md:ml-0'>
+                          <div className='border-[2px] rounded-full w-[70px] mt-2 mb-1 md:w-28 border-lime'/>
+                          <h2 className='text-[18px] font-semibold md:text-[30px] mb-4'>Services Bundle Offers</h2>
+                      </div>
+                      <PricingCarouselComponent />
+                      <Pricing />
                   </div>
-                  <PricingCarouselComponent />
-                  <Pricing />
-              </div>
-          </BoxBackgroundComponent>
+              </BoxBackgroundComponent>
 
-          {/*HOW IT WORKS*/}
-          <div className="w-full py-5 lg:w-[970px] xl:min-w-[1170px] xl:max-w-[1170px] mx-auto">
+              {/*HOW IT WORKS*/}
+
               <section>
                   <div className="w-full flex flex-col items-center justify-center px-3">
-                      <div className="  w-full md:container flex flex-col flex-wrap pb-[0px]">
-                          <div className="border-lime mb-[30px] py-2">
-                              <div className='border-b-4 rounded-full border-lime w-[85px] mb-3'></div>
+                      <div className="w-full md:container flex flex-col flex-wrap pb-[0px]">
+                          <div className=" border-lime mb-[30px] py-2">
+                              <div className="border-b-4 border-lime rounded-full w-[85px] mb-3"></div>
                               <h2 className="text-black text-4xl font-medium font-semibold">
                                   How it works?
                               </h2>
                           </div>
                           {/*<StepperComponent />*/}
-                          <div className="hidden mt-10 w-full md:flex  justify-between xl:flex-nowrap gap-1 xl:gap-7">
+                          <div className="hidden mt-10 w-full md:flex justify-center xl:flex-nowrap gap-1 xl:gap-7">
                               {howItWorks.map(({ title, paragraph, image }: any, index) => {
                                   return (
-                                    <HowItWorks
-                                      key={index}
-                                      title={title}
-                                      paragraph={paragraph}
-                                      index={index}
-                                      type="Home"
-                                      image={image}
-                                    />
+                                      <HowItWorks
+                                          key={index}
+                                          title={title}
+                                          paragraph={paragraph}
+                                          index={index}
+                                          type="Home"
+                                          image={image}
+                                      />
                                   );
                               })}
                           </div>
-                          <div className="flex mt-10  md:hidden w-full h-[300px] justify-center xl:flex-nowrap gap-1 xl:gap-7">
-                              <CarouselComponent list={howItWorks} component={HowItWorks} className='min-h-400px '/>
+                          <div className="flex mt-10  md:hidden w-full h-[300px] justify-center xl:flex-nowrap gap-1 lg:gap-7">
+                              <CarouselComponent list={howItWorks} component={HowItWorks} className='h-420px '/>
                           </div>
                       </div>
                   </div>
