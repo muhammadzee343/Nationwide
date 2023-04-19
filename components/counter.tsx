3c90fda86bdbaf80ccb94771b0cb6e4f4be7b87b
Asad Ali <muhammad.adil@piecyfer.com>
2023-04-19 17:33:38 +0500

    Counter changes.

--- components/counter.tsx
+++ components/counter.tsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 function Counter({
-  minValue = 0,
-  label,
-  setValue,
-  preValue = 0,
-  className = "",
-  containerClass = "w-5 h-5",
-  iconClass = "text-white",
-}: any) {
+                   minValue = 0,
+                   label,
+                   setValue,
+                   preValue = 0,
+                   className = "",
+                   containerClass = "w-5 h-5",
+                   iconClass = "text-white",
+                 }: any) {
   const [count, setCount] = useState<number>(preValue || minValue);
 
   useEffect(() => {
@@ -39,34 +39,40 @@ function Counter({
   return (
     <div className="flex cursor-pointer items-center">
       <div className="mr-2 flex items-center" onClick={decrement}>
-        <span
+        <a
           className={`bg-lime flex justify-center items-center items-center rounded-full ${containerClass}`}
         >
           <FontAwesomeIcon
             className={`w-[17px] h-[20px] ${iconClass}`}
             icon={faMinus}
           />
-        </span>
+        </a>
       </div>
       <input
-        className={`w-16 h-7 border border-grey-500 flex
-      justify-center text-center text-base text-dark-blue font-semibold outline-none focus:ring-transparent ${className} `}
+        className={`h-7 border border-grey-500 flex
+      justify-center text-center text-base text-dark-blue font-semibold outline-none focus:ring-transparent ${className} px-0 ${label ==='property_price' ? "min-w-16":"w-16"}`}
         type="number"
         step={1}
         min={minValue}
         value={count}
-        onChange={() => {}}
+        name={label}
+        onChange={(event) => {
+          const regex = /^[0-9]+$/;
+          if (regex.test(event.target.value)) {
+            setCount(parseInt(event.target.value))
+          }
+        }}
       />
 
       <div className="ml-2 flex items-center" onClick={increment}>
-        <span
+        <a
           className={`bg-lime flex justify-center items-center items-center rounded-full ${containerClass}`}
         >
           <FontAwesomeIcon
             className={`w-[17px] h-[20px] ${iconClass}`}
             icon={faPlus}
           />
-        </span>
+        </a>
       </div>
     </div>
   );
